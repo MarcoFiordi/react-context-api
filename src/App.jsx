63edeb58413b1532b36b123db97b6cbedcbf87c1
Marco Fiordi <marco.fiordi97@gmail.com>
2026-05-19 17:26:10 +0200

    wrappata app con Budget provider

--- src/App.jsx
+++ src/App.jsx
@@ -8,22 +8,25 @@ import Contacts from "./pages/Contacts";
 import Error404 from "./pages/Error404";
 import Prodotti from "./pages/Prodotti";
 import ProductDetail from "./pages/ProductDetail";
+import { BudgetProvider } from "./contexts/BudgetContext";
 
 
 function App() {
 
   return (
-    
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Homepage/>} />
-        <Route path="/contacts" element={<Contacts/>}/>
-        <Route path="/prodotti" element={<Prodotti/>}/>
-        <Route path="/prodotti/:id" element={<ProductDetail />} />
-        <Route path="*" element={<Error404/>} />
-      </Routes>
-    </BrowserRouter>
+    <BudgetProvider>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/contacts" element={<Contacts />} />
+          <Route path="/prodotti" element={<Prodotti />} />
+          <Route path="/prodotti/:id" element={<ProductDetail />} />
+          <Route path="*" element={<Error404 />} />
+        </Routes>
+      </BrowserRouter>
+    </BudgetProvider>
+
   )
 
 
